8d60debe4ace28e9c3965c43f6e4b8ab43f3c1f1
zhouxiaohua <zhouxiaohua@cnic.cn>
2017-06-14 15:47:57 +0800

    follower日志

--- servers/follower.go
+++ servers/follower.go
@@ -6,6 +6,7 @@ import (
 	"SimpleRaft/settings"
 	"math/rand"
 	"SimpleRaft/utils"
+	"log"
 )
 
 type Follower struct {
@@ -25,16 +26,20 @@ func (this *Follower) Init(role_chan chan RoleState) error {
 
 	this.chan_commits = make(chan int)
 	this.chan_timeout = make(chan bool)
+
+	log.Printf("FOLLOWER(%d)：初始化...\n", this.CurrentTerm)
+
 	return nil
 }
 
 func (this *Follower) StartAllService() {
-	go this.startLogApplService() //日志应用服务
 	go this.startTimeOutService() //计时服务
+	go this.startLogApplService() //日志应用服务
 }
 
 //定时服务，超时即切换到candidate状态
 func (this *Follower) startTimeOutService() {
+	log.Printf("FOLLOWER(%d)：启动计时服务...\n", this.CurrentTerm)
 	for {
 		if !this.active.IsSet() {
 			break
@@ -51,18 +56,21 @@ func (this *Follower) startTimeOutService() {
 			break
 		}
 	}
+	log.Printf("FOLLOWER(%d)：计时服务终止！！！\n", this.CurrentTerm)
 }
 
 //日志应用服务(更新lastApplied)
 func (this *Follower) startLogApplService() {
+	log.Printf("FOLLOWER(%d)：启动日志应用服务...\n", this.CurrentTerm)
 	for {
 		if !this.active.IsSet() {
-			return
+			break
 		}
 		for this.LastApplied < this.CommitIndex {
 			_log := this.Logs.Get(this.LastApplied + 1)
 			db.WriteToDisk(_log.Command)
 			this.LastApplied++
+			log.Printf("FOLLOWER(%d): %d 已写到日志文件\n", this.CurrentTerm, this.LastApplied)
 		}
 
 		select {
@@ -72,6 +80,7 @@ func (this *Follower) startLogApplService() {
 			//do nothing
 		}
 	}
+	log.Printf("FOLLOWER(%d)：日志应用服务终止！！！\n", this.CurrentTerm)
 }
 
 func (this *Follower) HandleVoteReq(args0 VoteReqArg, args1 *VoteAckArg) error {
@@ -79,10 +88,13 @@ func (this *Follower) HandleVoteReq(args0 VoteReqArg, args1 *VoteAckArg) error {
 		return nil
 	}
 
+	log.Printf("FOLLOWER(%d)：收到%s的投票请求...\n", this.CurrentTerm, args0.CandidateID)
+
 	//过期leader直接拒绝
 	if this.CurrentTerm > args0.Term {
 		args1.Term = this.CurrentTerm
 		args1.VoteGranted = false
+		log.Printf("FOLLOWER(%d)：%s为过期的CANDIDATE，拒绝为其投票！！！\n", this.CurrentTerm, args0.CandidateID)
 		return nil
 	}
 
@@ -110,9 +122,11 @@ func (this *Follower) HandleVoteReq(args0 VoteReqArg, args1 *VoteAckArg) error {
 		this.VotedFor = args0.CandidateID
 		args1.Term = this.CurrentTerm
 		args1.VoteGranted = true
+		log.Printf("FOLLOWER(%d)：投票给%s\n", this.CurrentTerm, args0.CandidateID)
 	} else {
 		args1.Term = this.CurrentTerm
 		args1.VoteGranted = false
+		log.Printf("FOLLOWER(%d)：不支持%s\n", this.CurrentTerm, args0.CandidateID)
 	}
 
 	return nil
@@ -138,6 +152,7 @@ func (this *Follower) HandleAppendLogReq(args0 LogAppArg, args1 *LogAckArg) erro
 	if args0.Term > this.CurrentTerm {
 		args1.Term = this.CurrentTerm
 		args1.Success = false
+		log.Printf("FOLLOWER(%d)：收到过期leader(%s)的append_log请求\n", this.CurrentTerm, args0.LeaderID)
 		return nil
 	}
 
@@ -147,6 +162,7 @@ func (this *Follower) HandleAppendLogReq(args0 LogAppArg, args1 *LogAckArg) erro
 
 	//来自leader的心跳信息(更新commit index)
 	if len(args0.Entries) == 0 {
+		log.Printf("FOLLOWER(%d)：收到leader(%s)的心跳信息\n", this.CurrentTerm, args0.LeaderID)
 		return this.handleHeartBeat(args0, args1)
 	}
 
@@ -155,6 +171,7 @@ func (this *Follower) HandleAppendLogReq(args0 LogAppArg, args1 *LogAckArg) erro
 	if args0.PreLogIndex > -1 {
 		preLog := this.Logs.Get(args0.PreLogIndex)
 		if preLog.Term != args0.PreLogTerm {
+			log.Printf("FOLLOWER(%d)：与leader(%s)的日志信息不一致\n", this.CurrentTerm, args0.LeaderID)
 			this.Logs.RemoveFrom(args0.PreLogIndex)
 			args1.Success = false
 			return nil
@@ -163,6 +180,9 @@ func (this *Follower) HandleAppendLogReq(args0 LogAppArg, args1 *LogAckArg) erro
 	lastLogIdx = this.Logs.Extend(args0.Entries)
 	args1.LastLogIndex = lastLogIdx
 	args1.Success = true
+
+	log.Printf("FOLLOWER(%d)：收到leader(%s)的新日志记录(PreTerm: %d, PreIndex: %d, Size: %d)\n",
+		this.CurrentTerm, args0.LeaderID, args0.PreLogTerm, args0.PreLogIndex, len(args0.Entries))
 
 	return nil
 }
@@ -192,9 +212,7 @@ func (this *Follower) handleHeartBeat(args0 LogAppArg, args1 *LogAckArg) error {
 					continue
 				}
 			}
-
 		}()
-
 	}
 	return nil
 }
