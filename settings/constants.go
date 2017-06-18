bb296916235913cce0fd6f17d3ad237e068585a1
zhouxiaohua <zhouxiaohua@cnic.cn>
2017-06-18 08:28:24 +0800

    更新heartbeat频率

--- settings/constants.go
+++ settings/constants.go
@@ -2,7 +2,9 @@ package settings
 
 const TIMEOUT_MIN = 1000 	//1s
 const TIMEOUT_MAX = 2000	//2s
-const HEART_BEATS = 1000 	//1s
+
+const HEART_BEATS = 300 	//500ms
+
 const COMMIT_WAIT = 1000	//1s
 const CLIENT_WAIT = 150		//150ms
 
